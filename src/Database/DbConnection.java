682aa6916a0a24c9d7eed08c8f28556db595efe2
imane-bouabidi <imaneimane20201@gmail.com>
2024-09-19 11:55:16 +0100

    Db classe

--- src/Database/DbConnection.java
+++ src/Database/DbConnection.java
@@ -1,4 +1,31 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/bati_cuisine";
+    private static final String USER = "postgres";
+    private static final String PASS = "Imane2002***";
+
+    public static DbConnection instance=new DbConnection();
+
+    private Connection connection;
+
+    private DbConnection() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException e) {
+            System.err.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
+    public static DbConnection getInstance() {
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
